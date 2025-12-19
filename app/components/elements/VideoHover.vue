a6a7496fe628d89c6f66c90f5eacdcea61565f15
Miqayel <miqayelpetrosyan@gmail.com>
2025-12-19 13:34:09 +0400

    change video hover logic

--- app/components/elements/VideoHover.vue
+++ app/components/elements/VideoHover.vue
@@ -1,11 +1,7 @@
 <script setup>
 const props = defineProps({
-  start: {
-    type: String,
-  },
-  hover: {
-    type: String,
-  },
+  start: String,
+  hover: String,
   classes: {
     type: String,
     default: '',
@@ -13,6 +9,12 @@ const props = defineProps({
 })
 
 const isHovered = ref(false)
+const isHoverPlaying = ref(false)
+
+const canHover = ref(false)
+const hasEnded = ref(false)
+const isMouseInside = ref(false)
+
 const containerRef = ref(null)
 const videoRef1 = ref(null)
 const videoRef2 = ref(null)
@@ -21,41 +23,89 @@ const {stop} = useIntersectionObserver(
     containerRef,
     ([{isIntersecting}]) => {
       if (isIntersecting) {
-        videoRef1.value?.play()
-        videoRef2.value?.play()
+        if (!hasEnded.value) {
+          videoRef1.value?.play()
+        }
       } else {
         videoRef1.value?.pause()
-        videoRef2.value?.pause()
       }
     },
     {threshold: 0.5}
 )
 
 onUnmounted(() => stop())
+
+const playHoverVideo = () => {
+  if (isHoverPlaying.value) return
+
+  isHoverPlaying.value = true
+  isHovered.value = true
+
+  const video = videoRef2.value
+  if (video) {
+    video.currentTime = 0
+    video.play()
+  }
+}
+
+const onStartEnded = () => {
+  hasEnded.value = true
+  canHover.value = true
+
+  if (isMouseInside.value) {
+    playHoverVideo()
+  }
+}
+
+const onHoverEnded = () => {
+  if (isMouseInside.value) {
+    const video = videoRef2.value
+    if (video) {
+      video.currentTime = 0
+      video.play()
+    }
+  } else {
+    isHoverPlaying.value = false
+    isHovered.value = false
+  }
+}
+
+const onMouseEnter = () => {
+  isMouseInside.value = true
+  if (canHover.value) {
+    playHoverVideo()
+  }
+}
+
+const onMouseLeave = () => {
+  isMouseInside.value = false
+}
 </script>
 
 <template>
   <div
       ref="containerRef"
-      @mouseenter="isHovered = true"
-      @mouseleave="isHovered = false"
+      @mouseenter="onMouseEnter"
+      @mouseleave="onMouseLeave"
   >
     <video
-        :class="classes"
         ref="videoRef1"
+        :class="classes"
         v-show="!isHovered"
         muted
-        loop
+        playsinline
+        @ended="onStartEnded"
     >
       <source :src="start" type="video/mp4"/>
     </video>
 
     <video
-        :class="classes"
         ref="videoRef2"
+        :class="classes"
         v-show="isHovered"
         muted
-        loop
+        playsinline
+        @ended="onHoverEnded"
     >
       <source :src="hover" type="video/mp4"/>
     </video>
